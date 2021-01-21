b8d496f5e136840a2b7f9ad7735602bdc9787a4f
yuta316 <iy.24.bump.09@gmail.com>
2021-01-21 09:09:53 +0000

    [add] add index_blade_php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <div class="posts">
+            <div class="post">
+                <h2 class="title">Title 1</h2>
+                <p class='body'>This is a sample</p>
+            </div>
+        </div>
+    </body>
+
+</html>
